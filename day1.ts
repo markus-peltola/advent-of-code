380bbf88ea7ff2de4c7a51ffee5eeaba105a128f
markus.peltola <ot1s87@gmail.com>
2022-12-01 21:39:52 +0200

    Cleaned code

--- day1.ts
+++ day1.ts
@@ -1,26 +1,6 @@
 import * as fs from 'fs';
 
-async function part1() {
-  const input = fs.readFileSync('input/day1_1.txt');
-  const rows = input.toString().split('\r\n');
-
-  let mostCalories = 0;
-  let currentElfCalories = 0;
-
-  for (const row of rows) {
-    if (row) {
-      currentElfCalories += parseInt(row);
-    } else {
-      if (currentElfCalories > mostCalories) mostCalories = currentElfCalories;
-      currentElfCalories = 0;
-    }
-  }
-  if (currentElfCalories > mostCalories) mostCalories = currentElfCalories;
-
-  console.log(mostCalories);
-}
-
-async function part2() {
+async function main() {
   const input = fs.readFileSync('input/day1_1.txt');
   const rows = input.toString().split('\r\n');
 
@@ -40,9 +20,11 @@ async function part2() {
     return a - b;
   });
 
+  // Answer to part 1
+  console.log(caloriesList[caloriesList.length - 1]);
+  // Answer to part 2
   console.log(caloriesList[caloriesList.length - 1] + caloriesList[caloriesList.length - 2] + caloriesList[caloriesList.length - 3]);
-  // console.log(caloriesList[caloriesList.length - 1]);
 }
 
 // part1();
-part2();
+main();
