105e73a4928810e6e4551fd721080e9348c85fbf
Markus Peltola <markus.peltola@rantalainen.fi>
2024-12-03 15:08:27 +0200

    Day 03 complete

--- day02.ts
+++ day02.ts
@@ -1,23 +1,119 @@
 import fs from 'fs';
 
-const useTestInput = true;
+const useTestInput = false;
 
 const inputFile = fs.readFileSync('input/day02.txt');
 
-const testInput: string = ``
+const testInput: string = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
 
-let inputRows: string[];
+let reports: string[];
 if (useTestInput) {
-  inputRows = testInput.split('\n');
+  reports = testInput.split('\n');
 } else {
-  inputRows = inputFile.toString().split('\n').slice(0, -1);
+  reports = inputFile.toString().split('\n').slice(0, -1);
 }
 
 async function part1() {
+  let safeReports = 0;
+  for (const report of reports) {
+    const levels = report.split(' ').map((level) => parseInt(level));
+    let safeReport = true;
+    let direction: 'inc' | 'dec';
+    // If there is no direction, report is unsafe
+    if (levels[0] === levels[1]) continue;
+    if (levels[1] - levels[0] < 0) direction = 'dec';
+    else direction = 'inc';
+
+    for (let x = 1; x < levels.length; x++) {
+      let levelChange: number;
+      if (direction === 'inc') {
+        levelChange = levels[x] - levels[x - 1];
+      } else {
+        levelChange = levels[x - 1] - levels[x];
+      }
+      // Level change should be between 1 and 3 to be safe
+      if (levelChange < 1 || levelChange > 3) {
+        safeReport = false;
+        break;
+      }
+    }
+
+    if (safeReport) safeReports++;
+  }
+
+  console.log(safeReports);
 }
 
 async function part2() {
+  const logStream = fs.createWriteStream('logs/day02_part2.txt', { flags: 'a' });
+  let safeReports = 0;
+  for (const report of reports) {
+    const levels = report.split(' ').map((level) => parseInt(level));
+    // logStream.write(`Testing report: ${levels}\n`);
+    let currentReportSafe = false;
+
+    if (testIncreasingLevels(levels, false, logStream)) {
+      safeReports++;
+      currentReportSafe = true;
+      continue;
+    }
+
+    if (testDecreasingLevels(levels, false, logStream)) {
+      safeReports++;
+      currentReportSafe = true;
+      continue;
+    }
+    logStream.write(`${levels}\n`);
+  }
+
+  console.log(safeReports);
+  logStream.end();
 }
 
-part1();
-// part2();
+function testIncreasingLevels(levels: number[], lifelineUsed: boolean, logStream: fs.WriteStream): boolean {
+  for (let x = 1; x < levels.length; x++) {
+    const levelChange = levels[x] - levels[x - 1];
+    if (levelChange < 1 || levelChange > 3) {
+      if (!lifelineUsed) {
+        const lifelineLevels = [...levels.slice(0, x - 1), ...levels.slice(x)];
+        if (testIncreasingLevels(lifelineLevels, true, logStream)) {
+          // logStream.write(`Report ${lifelineLevels} found to be safe when Problem Dampener removed ${levels[x - 1]} at index ${x - 1}\n`);
+          return true;
+        } else if (testIncreasingLevels(levels.slice(0, -1), true, logStream)) {
+          // logStream.write(`Report ${levels.slice(0, -1)} found to be safe when Problem Dampener removed the last level\n`);
+          return true;
+        } else return false;
+      } else return false;
+    }
+  }
+  // logStream.write(`Report ${levels} found to be safe.\n`);
+  return true;
+}
+
+function testDecreasingLevels(levels: number[], lifelineUsed: boolean, logStream: fs.WriteStream): boolean {
+  for (let x = 1; x < levels.length; x++) {
+    const levelChange = levels[x - 1] - levels[x];
+    if (levelChange < 1 || levelChange > 3) {
+      if (!lifelineUsed) {
+        const lifelineLevels = [...levels.slice(0, x - 1), ...levels.slice(x)];
+        if (testDecreasingLevels(lifelineLevels, true, logStream)) {
+          // logStream.write(`Report ${lifelineLevels} found to be safe when Problem Dampener removed ${levels[x - 1]} at index ${x - 1}\n`);
+          return true;
+        } else if (testDecreasingLevels(levels.slice(0, -1), true, logStream)) {
+          // logStream.write(`Report ${levels.slice(0, -1)} found to be safe when Problem Dampener removed the last level\n`);
+          return true;
+        } else return false;
+      } else return false;
+    }
+  }
+  // logStream.write(`Report ${levels} found to be safe.\n`);
+  return true;
+}
+
+// part1();
+part2();
--- day03.ts
+++ day03.ts
@@ -1,23 +1,77 @@
 import fs from 'fs';
 
-const useTestInput = true;
+const useTestInput = false;
 
-const inputFile = fs.readFileSync('input/day02.txt');
+const inputFile = fs.readFileSync('input/day03.txt');
 
-const testInput: string = ``
+const testInput: string = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 
-let inputRows: string[];
+let inputText: string;
 if (useTestInput) {
-  inputRows = testInput.split('\n');
+  inputText = testInput;
 } else {
-  inputRows = inputFile.toString().split('\n').slice(0, -1);
+  inputText = inputFile.toString();
 }
 
 async function part1() {
+  const regExp = new RegExp(/mul\(\d{1,3},\d{1,3}\)/g);
+  const mulFunctions = inputText.match(regExp);
+  if (!mulFunctions?.length) return;
+
+  let total = 0;
+  for (const mulFunc of mulFunctions) {
+    const numbers = mulFunc.match(/\d{1,3},\d{1,3}/)![0];
+    const firstNum = parseInt(numbers.split(',')[0]);
+    const secondNum = parseInt(numbers.split(',')[1]);
+    total += firstNum * secondNum;
+  }
+  console.log(total);
 }
 
 async function part2() {
+  const regExp_func = new RegExp(/mul\(\d{1,3},\d{1,3}\)/g);
+  const regExp_enable = new RegExp(/do\(\)/g);
+  const regExp_disable = new RegExp(/don\'t\(\)/g);
+
+  const funcIndexes: { func: string; index: number }[] = [];
+  const enableIndexes: number[] = [];
+  const disableIndexes: number[] = [];
+
+  while (true) {
+    const foundFunc = regExp_func.exec(inputText);
+    if (!foundFunc || !regExp_func.lastIndex) break;
+    funcIndexes.push({ func: foundFunc.toString(), index: foundFunc?.index });
+  }
+
+  while (true) {
+    const foundEnable = regExp_enable.exec(inputText);
+    if (foundEnable) enableIndexes.push(foundEnable?.index);
+    if (!regExp_enable.lastIndex) break;
+  }
+
+  while (true) {
+    const foundDisable = regExp_disable.exec(inputText);
+    if (foundDisable) disableIndexes.push(foundDisable?.index);
+    if (!regExp_disable.lastIndex) break;
+  }
+
+  let total = 0;
+  let enabled = true;
+
+  for (let i = 0; i < inputText.length; i++) {
+    if (enableIndexes.includes(i)) enabled = true;
+    if (disableIndexes.includes(i)) enabled = false;
+
+    if (funcIndexes.some((item) => item.index === i) && enabled) {
+      const numbers = funcIndexes.find((item) => item.index === i)?.func.match(/\d{1,3},\d{1,3}/)![0];
+      const firstNum = parseInt(numbers!.split(',')[0]);
+      const secondNum = parseInt(numbers!.split(',')[1]);
+      total += firstNum * secondNum;
+    }
+  }
+
+  console.log(total);
 }
 
-part1();
-// part2();
+// part1();
+part2();
