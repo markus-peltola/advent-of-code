6bae65bc0b803c36ac41766014e132e929220edd
markus-peltola <ot1s87@gmail.com>
2024-12-07 13:53:25 +0200

    Completed day 7

--- day06.ts
+++ day06.ts
@@ -1,10 +1,10 @@
-import fs from "fs";
+import fs from 'fs';
 
-const useTestInput = true;
+const useTestInput = false;
 
-// 5525 too low
+// 1778 too low
 
-const inputFile = fs.readFileSync("input/day06.txt");
+const inputFile = fs.readFileSync('input/day06.txt');
 
 const testInput: string = `....#.....
 .........#
@@ -19,70 +19,68 @@ const testInput: string = `....#.....
 
 let inputRows: string[];
 if (useTestInput) {
-  inputRows = testInput.split("\n");
+  inputRows = testInput.split('\n');
 } else {
-  inputRows = inputFile.toString().split("\n").slice(0, -1);
+  inputRows = inputFile.toString().split('\n');
 }
-const map = inputRows.map((row) => row.split(""));
-const mapHeight = map.length;
-const mapWidth = map[0].length;
+const originalMap = inputRows.map((row) => row.split(''));
+const mapHeight = originalMap.length;
+const mapWidth = originalMap[0].length;
 
-type Direction = "N" | "E" | "S" | "W";
+type Direction = 'N' | 'E' | 'S' | 'W';
 type GuardPosition = {
   position: { x: number; y: number };
   direction: Direction;
 };
 
 function getGuardPosition(): GuardPosition {
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === "^") return { position: { x, y }, direction: "N" };
+  for (let y = 0; y < originalMap.length; y++) {
+    for (let x = 0; x < originalMap[y].length; x++) {
+      if (originalMap[y][x] === '^') return { position: { x, y }, direction: 'N' };
     }
   }
-  throw new Error("Could not locate the guard");
+  throw new Error('Could not locate the guard');
 }
 
 function move(guard: GuardPosition, map: string[][]): GuardPosition {
   const x = guard.position.x;
   const y = guard.position.y;
-  if (guard.direction === "N") {
-    if (y > 0 && map[y - 1][x] === "#")
-      return { position: { y, x: x + 1 }, direction: "E" };
-    else return { position: { y: y - 1, x }, direction: "N" };
+  if (guard.direction === 'N') {
+    if (y > 0 && map[y - 1][x] === '#') return { position: { y, x }, direction: 'E' };
+    else return { position: { y: y - 1, x }, direction: 'N' };
   }
-  if (guard.direction === "E") {
-    if (x < mapWidth - 1 && map[y][x + 1] === "#")
-      return { position: { y: y + 1, x }, direction: "S" };
-    else return { position: { y, x: x + 1 }, direction: "E" };
+  if (guard.direction === 'E') {
+    if (x < mapWidth - 1 && map[y][x + 1] === '#') return { position: { y, x }, direction: 'S' };
+    else return { position: { y, x: x + 1 }, direction: 'E' };
   }
-  if (guard.direction === "S") {
-    if (y < mapHeight - 1 && map[y + 1][x] === "#")
-      return { position: { y, x: x - 1 }, direction: "W" };
-    else return { position: { y: y + 1, x }, direction: "S" };
+  if (guard.direction === 'S') {
+    if (y < mapHeight - 1 && map[y + 1][x] === '#') return { position: { y, x }, direction: 'W' };
+    else return { position: { y: y + 1, x }, direction: 'S' };
   }
-  if (guard.direction === "W") {
-    if (x > 0 && map[y][x - 1] === "#")
-      return { position: { y: y - 1, x }, direction: "N" };
-    else return { position: { y, x: x - 1 }, direction: "W" };
+  if (guard.direction === 'W') {
+    if (x > 0 && map[y][x - 1] === '#') return { position: { y, x }, direction: 'N' };
+    else return { position: { y, x: x - 1 }, direction: 'W' };
   }
-  throw new Error("This has to be done to make Typescript shut up about it.");
+  throw new Error('This has to be done to make Typescript shut up about it.');
 }
 
 function countXs(): number {
   let total = 0;
-  for (const row of map) {
+  for (const row of originalMap) {
     for (const col of row) {
-      if (col === "X") total++;
+      if (col === 'X') total++;
     }
   }
   return total;
 }
 
+const guardStartingPosition = getGuardPosition();
+
 async function part1() {
   let guardPosition = getGuardPosition();
   while (true) {
-    map[guardPosition.position.y][guardPosition.position.x] = "X";
-    guardPosition = move(guardPosition, map);
+    originalMap[guardPosition.position.y][guardPosition.position.x] = 'X';
+    guardPosition = move(guardPosition, originalMap);
     if (
       guardPosition.position.y < 0 ||
       guardPosition.position.y >= mapHeight ||
@@ -96,24 +94,18 @@ async function part1() {
 }
 
 async function part2() {
-  const maxRounds = 100000;
+  const maxRounds = 10000;
   let total = 0;
-  const guardStartingPosition = getGuardPosition();
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      // If there is already block or it's the guard's starting position, skip to next
-      if (
-        map[y][x] === "#" ||
-        (y === guardStartingPosition.position.y &&
-          x === guardStartingPosition.position.x)
-      )
-        continue;
+  for (let y = 0; y < mapHeight; y++) {
+    for (let x = 0; x < mapWidth; x++) {
+      // Use X's from part 1 to decide where to place the blocks
+      if (originalMap[y][x] !== 'X') continue;
       // Reset guard position
       let guardPosition = guardStartingPosition;
       // Get a fresh copy of the map
-      let mapCopy = map.slice();
+      let mapCopy = originalMap.map((rows) => [...rows]);
       // Place a new block on the map
-      mapCopy[y][x] = "#";
+      mapCopy[y][x] = '#';
       // Reset the rounds
       let round = 1;
       while (true) {
@@ -129,7 +121,6 @@ async function part2() {
         }
         // ...or max round is reached, when it (probably) got stuck in endless loop
         if (round > maxRounds) {
-          console.log(`Block placement at (${x},${y}) lead to endless loop.`);
           total++;
           break;
         }
@@ -140,5 +131,5 @@ async function part2() {
   console.log(total);
 }
 
-// part1();
+part1();
 part2();
--- day07.ts
+++ day07.ts
@@ -1,21 +1,104 @@
-import fs from "fs";
+import fs from 'fs';
 
-const useTestInput = true;
+const useTestInput = false;
 
-const inputFile = fs.readFileSync("input/day07.txt");
+const inputFile = fs.readFileSync('input/day07.txt');
 
-const testInput: string = ``;
+const testInput: string = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
 
 let inputRows: string[];
 if (useTestInput) {
-  inputRows = testInput.split("\n");
+  inputRows = testInput.split('\n');
 } else {
-  inputRows = inputFile.toString().split("\n").slice(0, -1);
+  inputRows = inputFile.toString().split('\n');
 }
 
-async function part1() {}
+type Equation = {
+  testValue: number;
+  numbers: number[];
+};
 
-async function part2() {}
+function testEquation(equation: Equation): boolean {
+  const sum = equation.numbers[0] + equation.numbers[1];
+  const product = equation.numbers[0] * equation.numbers[1];
 
-part1();
-// part2();
+  if (equation.numbers.length === 2) {
+    if (sum === equation.testValue || product === equation.testValue) return true;
+    else return false;
+  } else {
+    const sumTree = testEquation({ testValue: equation.testValue, numbers: [sum, ...equation.numbers.slice(2)] });
+    if (sumTree) return true;
+    const multiplyTree = testEquation({ testValue: equation.testValue, numbers: [product, ...equation.numbers.slice(2)] });
+    if (multiplyTree) return true;
+  }
+  return false;
+}
+
+function part1() {
+  const equations: Equation[] = inputRows.map((row) => {
+    return {
+      testValue: parseInt(row.split(':')[0]),
+      numbers: row
+        .split(':')[1]
+        .trim()
+        .split(' ')
+        .map((num) => parseInt(num))
+    };
+  });
+
+  let total = 0;
+
+  for (const equation of equations) {
+    if (testEquation(equation)) total += equation.testValue;
+  }
+  console.log(total);
+}
+
+function testEquation2(equation: Equation): boolean {
+  const sum = equation.numbers[0] + equation.numbers[1];
+  const product = equation.numbers[0] * equation.numbers[1];
+  const concat = parseInt(equation.numbers[0].toString() + equation.numbers[1].toString());
+
+  if (equation.numbers.length === 2) {
+    if (sum === equation.testValue || product === equation.testValue || concat === equation.testValue) return true;
+    else return false;
+  } else {
+    const sumTree = testEquation2({ testValue: equation.testValue, numbers: [sum, ...equation.numbers.slice(2)] });
+    if (sumTree) return true;
+    const multiplyTree = testEquation2({ testValue: equation.testValue, numbers: [product, ...equation.numbers.slice(2)] });
+    if (multiplyTree) return true;
+    const concatTree = testEquation2({ testValue: equation.testValue, numbers: [concat, ...equation.numbers.slice(2)] });
+    if (concatTree) return true;
+  }
+  return false;
+}
+function part2() {
+  const equations: Equation[] = inputRows.map((row) => {
+    return {
+      testValue: parseInt(row.split(':')[0]),
+      numbers: row
+        .split(':')[1]
+        .trim()
+        .split(' ')
+        .map((num) => parseInt(num))
+    };
+  });
+
+  let total = 0;
+
+  for (const equation of equations) {
+    if (testEquation2(equation)) total += equation.testValue;
+  }
+  console.log(total);
+}
+
+// part1();
+part2();
