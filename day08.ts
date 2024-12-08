68d78d1053aa4834c5503db22aae92b7019af185
markus-peltola <ot1s87@gmail.com>
2024-12-08 15:30:39 +0200

    Day 8 part 1 complete

--- day08.ts
+++ day08.ts
@@ -1,10 +1,21 @@
 import fs from 'fs';
 
-const useTestInput = true;
+const useTestInput = false;
 
 const inputFile = fs.readFileSync('input/day08.txt');
 
-const testInput: string = ``;
+const testInput: string = `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`;
 
 let inputRows: string[];
 if (useTestInput) {
@@ -13,9 +24,106 @@ if (useTestInput) {
   inputRows = inputFile.toString().split('\n').slice(0, -1);
 }
 
-async function part1() {}
+type Point = { x: number; y: number };
+type AntennaCoords = {
+  type: string,
+  coords: Point[]
+}
 
-async function part2() {}
+const map = inputRows.map(row => row.split(''));
+const mapHeight = map.length;
+const mapWidth = map[0].length;
 
-part1();
-// part2();
+function getAntinodePoints(point1: Point, point2: Point): Point[] {
+  const x1 = point1.x;
+  const x2 = point2.x;
+  const y1 = point1.y;
+  const y2 = point2.y;
+
+  let antinode1: Point;
+  let antinode2: Point;
+
+  if (y2 > y1 && x2 > x1 || y2 < y1 && x2 < x1) {
+    const dx = Math.abs(x1 - x2);
+    const dy = Math.abs(y1 - y2);
+
+    antinode1 = { x: Math.min(x1, x2) - dx, y: Math.min(y1, y2) - dy }
+    antinode2 = { x: Math.max(x1, x2) + dx, y: Math.max(y1, y2) + dy }
+  } else {
+    const dx = Math.abs(x1 - x2);
+    const dy = Math.abs(y1 - y2);
+
+    antinode1 = { x: Math.max(x1, x2) + dx, y: Math.min(y1, y2) - dy }
+    antinode2 = { x: Math.min(x1, x2) - dx, y: Math.max(y1, y2) + dy }
+  }
+
+  const returnArray = [];
+  if (antinode1.x >= 0 && antinode1.x < mapWidth && antinode1.y >= 0 && antinode1.y < mapHeight) {
+    returnArray.push(antinode1);
+  }
+  if (antinode2.x >= 0 && antinode2.x < mapWidth && antinode2.y >= 0 && antinode2.y < mapHeight) {
+    returnArray.push(antinode2);
+  }
+
+  return returnArray;
+}
+
+function getAntennaCoords(map: string[][]): AntennaCoords[] {
+  const antennaCoords: AntennaCoords[] = [];
+  // Get all the different kind of antennas from the map
+  const antennaTypes = new Set<string>();
+  for (let y = 0; y < mapHeight; y++) {
+    for (let x = 0; x < mapWidth; x++) {
+      if (map[y][x] !== '.') antennaTypes.add(map[y][x]);
+    }
+  }
+
+  // Get all the coords for different antennas from the map
+  for (const antenna of antennaTypes) {
+    antennaCoords.push({ type: antenna, coords: [] });
+    for (let y = 0; y < mapHeight; y++) {
+      for (let x = 0; x < mapWidth; x++) {
+        if (map[y][x] === antenna) antennaCoords.find(antennaCoord => antennaCoord.type === antenna)!.coords.push({ y, x });
+      }
+    }
+  }
+  return antennaCoords;
+}
+
+function generateUniquePairs(coords: Point[]): [Point, Point][] {
+  const pairs: [Point, Point][] = [];
+
+  for (let i = 0; i < coords.length; i++) {
+    for (let j = i + 1; j < coords.length; j++) {
+      pairs.push([coords[i], coords[j]]);
+    }
+  }
+
+  return pairs;
+}
+
+function part1() {
+  const antennaCoords = getAntennaCoords(map);
+  const antinodes: Point[] = [];
+  for (const antCoords of antennaCoords) {
+    console.log(`Calculating antinodes for antenna: ${antCoords.type}`);
+    const coordPairs = generateUniquePairs(antCoords.coords);
+    for (const coordPair of coordPairs) {
+      const coordPairAntinodes = getAntinodePoints(coordPair[0], coordPair[1]);
+      for (const antinodeCandidate of coordPairAntinodes) {
+        if (antinodes.some(antinode => antinode.x === antinodeCandidate.x && antinode.y === antinodeCandidate.y)) continue;
+        antinodes.push(antinodeCandidate);
+      }
+    }
+    console.log(`Antinodes so far: ${antinodes.length}`);
+  }
+  console.log(`Total antinodes: ${antinodes.length}`);
+}
+
+function part2() {
+  const antennaCoords = getAntennaCoords(map);
+  const antinodes: Point[] = [];
+}
+
+// part1();
+part2();
---
+++ day09.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const useTestInput = true;
+
+const inputFile = fs.readFileSync('input/day09.txt');
+
+const testInput: string = ``;
+
+let inputRows: string[];
+if (useTestInput) {
+  inputRows = testInput.split('\n');
+} else {
+  inputRows = inputFile.toString().split('\n').slice(0, -1);
+}
+
+async function part1() { }
+
+async function part2() { }
+
+part1();
+// part2();
